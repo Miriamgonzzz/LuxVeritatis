9b05d0d577a39d4391305f37a31be88045f59443
Francisco Albiar Jiménez <francisco.albiar@hotmail.com>
2025-05-01 12:39:59 +0200

    Añadido script para la rotación de los objetos en el inspector

---
+++ Assets/Scripting/ObjectRotator.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour
+{
+    //velocidad de rotación
+    public float rotationSpeed = 100f;
+
+    void Update()
+    {
+        //permite rotar el objeto mientras mantenemos pulsado el botón izquierdo del ratón
+        if (Input.GetMouseButton(0))
+        {
+            float rotX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            float rotY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+
+            transform.Rotate(Vector3.up, -rotX, Space.World);
+            transform.Rotate(Vector3.right, rotY, Space.World);
+        }
+    }
+}
